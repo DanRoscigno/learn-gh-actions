a58377f8f0001abd0eec43be658b59d6cf570736
Dan Roscigno <dan@roscigno.com>
2022-06-02 13:54:43 -0400

    set baseURL

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://danroscigno.github.io',
-  baseUrl: '/learn-gh-actions',
+  baseUrl: '/learn-gh-actions/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
