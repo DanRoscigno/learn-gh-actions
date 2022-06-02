44b3972421f80a6d3617d12325dce85b7f34ec54
Dan Roscigno <dan@roscigno.com>
2022-06-02 12:33:16 -0400

    add GitHub Pages bits

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,8 +16,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'DanRoscigno', // Usually your GitHub org/user name.
+  projectName: 'learn-gh-actions', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // branch to deploy to
+  trailingSlash: 'false', // GitHub Pages adds a slash unless this is false
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
